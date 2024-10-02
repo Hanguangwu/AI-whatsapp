2d8e1c062fe1616937c6a6a78cb21c9311f08d8f
Hanguangwu <162727297+Hanguangwu@users.noreply.github.com>
2024-10-02 22:40:09 +0800

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{ hostname: "hallowed-whale-712.convex.cloud" }
+		],
+	},
+};
+
+export default nextConfig;
